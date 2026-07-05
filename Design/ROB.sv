306650c11ec2a995a03ca12ca00fe64aaa21adbb
Mutahir <mutahirahmed001@gmail.com>
2026-07-05 21:07:13 +0500

    added ROB

---
+++ Design/ROB.sv
@@ -0,0 +1,76 @@
+module ROB #(
+    parameter PRF_ADDRESS = 6,
+    parameter BIQ_ADDRESS = 5,
+    parameter ROB_SIZE = 64,
+    parameter ROB_PTR_SIZE = $clog2(ROB_SIZE)
+
+) (
+    input logic CLK, reset, stall_frontend, branch_mispredicted, rn_valid1, rn_valid2, 
+    input logic cdb_done1, cdb_done2,
+    input logic [PRF_ADDRESS-1:0]  rn_old_prd1, rn_old_prd2,   
+    input logic [ROB_PTR_SIZE-1:0] cdb_rob_index1, cdb_rob_index2, branch_rob_index,          
+    input logic [BIQ_ADDRESS-1:0] rn_biq_address,
+
+    output logic rob_full,
+    output logic [BIQ_ADDRESS-1:0] dis_biq_address, 
+    output logic [ROB_PTR_SIZE-1:0] current_rob_index,
+    output logic [PRF_ADDRESS-1:0] dis_free_old_prd1, dis_free_old_prd2
+);
+
+    typedef struct packed {
+        logic done;
+        logic [PRF_ADDRESS-1:0] old_prd;
+        logic [BIQ_ADDRESS-1:0] biq_address;
+    } ROB_organization;
+
+    
+    ROB_organization ROB [ROB_SIZE-1:0];
+    logic [ROB_PTR_SIZE:0] rob_head_ptr, rob_tail_ptr;
+    logic [ROB_PTR_SIZE:0] rob_count;
+    logic branch_wrap_bit; //to find what should be at msb when recovery of tail pointer on misprediction
+
+    assign branch_wrap_bit      = rob_head_ptr[ROB_PTR_SIZE] ^ (branch_rob_index < rob_head_ptr[ROB_PTR_SIZE-1:0]);
+    assign rob_count            = rob_tail_ptr - rob_head_ptr; //it would never be negative cuz taking ptrsize + 1
+    assign current_rob_index    = rob_tail_ptr[ROB_PTR_SIZE-1:0];
+    assign rob_full             = (rob_count >= (ROB_PTR_SIZE+1)'(ROB_SIZE - 1));          
+    assign completed1           = (rob_count > 0) && ROB[rob_head_ptr[ROB_PTR_SIZE-1:0]].done;
+    assign completed2           = (rob_count > 1) && ROB[ROB_PTR_SIZE'(rob_head_ptr[ROB_PTR_SIZE-1:0]+1)].done && completed1;
+    assign dis_free_old_prd1    = ROB[rob_head_ptr[ROB_PTR_SIZE-1:0]].old_prd;
+    assign dis_free_old_prd2    = ROB[ROB_PTR_SIZE'(rob_head_ptr[ROB_PTR_SIZE-1:0]+1)].old_prd;
+
+
+    always_ff @(posedge CLK) begin
+        if (reset) begin
+            rob_head_ptr <= 0;
+            rob_tail_ptr <= 0;
+        end
+        else begin
+            rob_head_ptr <= rob_head_ptr + { {(ROB_PTR_SIZE){1'b0}}, completed1 } 
+                                         + { {(ROB_PTR_SIZE){1'b0}}, completed2 };
+            if (branch_mispredicted) begin
+                rob_tail_ptr <= {branch_wrap_bit, branch_rob_index} + 1'b1;
+                dis_biq_address <= ROB[branch_rob_index].biq_address;
+            end
+            else if(!stall_frontend) begin
+                rob_tail_ptr <= rob_tail_ptr + { {(ROB_PTR_SIZE){1'b0}}, rn_valid1 } 
+                                             + { {(ROB_PTR_SIZE){1'b0}}, rn_valid2 };
+            end
+            if (cdb_done1) begin
+                ROB[cdb_rob_index1].done <= 1;
+            end
+            if (cdb_done2) begin
+                ROB[cdb_rob_index2].done <= 1;
+            end
+            if(!rob_full) begin
+                //for 1st instruction
+                ROB[rob_tail_ptr[ROB_PTR_SIZE-1:0]].done          <= 0;
+                ROB[rob_tail_ptr[ROB_PTR_SIZE-1:0]].old_prd       <= (rn_valid1)? rn_old_prd1 : rn_old_prd2;
+                ROB[rob_tail_ptr[ROB_PTR_SIZE-1:0]].biq_address   <= rn_biq_address;
+                //for 2nd instruction
+                ROB[ROB_PTR_SIZE'(rob_tail_ptr[ROB_PTR_SIZE-1:0]+1)].done        <= 0;
+                ROB[ROB_PTR_SIZE'(rob_tail_ptr[ROB_PTR_SIZE-1:0]+1)].old_prd     <= rn_old_prd2;
+                ROB[ROB_PTR_SIZE'(rob_tail_ptr[ROB_PTR_SIZE-1:0]+1)].biq_address <= rn_biq_address;
+            end
+        end
+    end
+endmodule
